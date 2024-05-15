ca40ababfd7f515e9fd476b3cd265f653f72e5f7
allahbin <16008@theling.cn>
2024-05-16 00:23:55 +0800

    修改导出

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
-export { default as ATiptap, mockImgUploader } from './ATiptap';
+export { mockImgUploader } from './ATiptap';
 export type { IATiptapProps } from './ATiptap';
 
 export type { IContent2, IMark, ITiptapJson } from './tide';
 
 export { getUrlParams, jsonToDom, paragraphTypes } from './utils/TiptapUtils';
+
+import ATiptap from './ATiptap';
+
+export default ATiptap;
