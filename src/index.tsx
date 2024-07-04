729935365f0b5b800715b912595edf194ac5fffb
allahbin <16008@theling.cn>
2024-07-04 18:00:46 +0800

    导出generateDirectoryTree

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ export type { IATiptapProps } from './ATiptapEdit';
 
 export type { IContent2, IMark, ITiptapJson } from './tide';
 
-export { jsonToDom, paragraphTypes } from './utils/TiptapUtils';
+export {
+  generateDirectoryTree,
+  jsonToDom,
+  paragraphTypes,
+} from './utils/TiptapUtils';
 
-export { default as TiptapRender, getUrlParams, type IRenderConfig } from './utils/TiptapRender';
+export {
+  default as TiptapRender,
+  getUrlParams,
+  type IRenderConfig,
+} from './utils/TiptapRender';
 export { default as TiptapRenderFactory } from './utils/TiptapRenderFactory';
 
 import ATiptap from './ATiptap';
